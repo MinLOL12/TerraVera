71f67eb75eabaecb0fa7f0a958f2137ae5190a01
MinLOL12 <302282839+MinLOL12@users.noreply.github.com>
2026-07-28 03:19:22 +0000

    fix: add missing textures and models for the gardening update
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/main/java/com/terravera/client/model/GreenhouseModel.java
+++ src/main/java/com/terravera/client/model/GreenhouseModel.java
@@ -6,6 +6,7 @@
 package com.terravera.client.model;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.model.GeoModel;
 import com.terravera.TerraVera;
@@ -25,14 +26,24 @@ public class GreenhouseModel<T extends GeoAnimatable> extends GeoModel<T> {
         this("cold_frame");
     }
 
+    private String getActualVariant(T animatable) {
+        if (animatable instanceof BlockEntity be) {
+            String name = net.minecraft.core.registries.BuiltInRegistries.BLOCK.getKey(be.getBlockState().getBlock()).getPath();
+            if (name.equals("cold_frame") || name.equals("hoop_house") || name.equals("glass_greenhouse") || name.equals("modern_greenhouse")) {
+                return name;
+            }
+        }
+        return variant;
+    }
+
     @Override
     public ResourceLocation getModelResource(T animatable) {
-        return TerraVera.identifier("geo/" + variant + ".geo.json");
+        return TerraVera.identifier("geo/" + getActualVariant(animatable) + ".geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(T animatable) {
-        return TerraVera.identifier("textures/block/" + variant + ".png");
+        return TerraVera.identifier("textures/block/" + getActualVariant(animatable) + ".png");
     }
 
     @Override
